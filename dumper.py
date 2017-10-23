92f54b76c4211d43dc37b6b7c76034d06ade5d19
iGio90 <giovanni.rocca.90@gmail.com>
2017-10-23 17:14:02 +0200

    Added option and modes

--- dumper.py
+++ dumper.py
@@ -4,9 +4,20 @@ import sys
 import time
 import os
 
-game = 0
-if len(sys.argv) > 1:
-    game = sys.argv[1]
+if len(sys.argv) < 3:
+    print("Usage: python3 dumper.py mode game")
+    print("")
+    print("Modes:")
+    print("0: Connect to local node proxy server")
+    print("1: Dump session")
+    print("")
+    print("Games:")
+    print("0: Boom Beach")
+    print("1: Clash of Clans")
+    exit(0)
+
+game = sys.argv[1]
+mode = sys.argv[2]
 
 package_name = ""
 if game == "0":
@@ -17,29 +28,30 @@ elif game == "1":
 def parse_message(message, data):
     payload = message["payload"]
     arr = payload.split("::::")
-    #sess_file.write(payload + "\n")
 
-    msglen = len(payload)
-    totalsent = 0
-    while totalsent < msglen:
-        sent = clientsocket.send(payload[totalsent:].encode())
-        if sent == 0:
-            raise RuntimeError("socket connection broken")
-        totalsent = totalsent + sent
-    if arr[0] == "0":
-        print("PK:"+arr[1])
-    elif arr[0] == "1":
-        print("SK:"+arr[1])
-    elif arr[0] == "2":
-        print("PKS:"+arr[1])
-    elif arr[0] == "3":
-        msgId = int(arr[1][:4], 16)
-        print("[CLIENT] " + str(msgId))
-    elif arr[0] == "4":
-        msgId = int(arr[1][:4], 16)
-        print("[SERVER] " + str(msgId) + " " + str(totalsent))
-
-    time.sleep(50.0 / 1000.0)
+    if mode == "1":
+        sess_file.write(payload + "\n")
+    elif mode == "0":
+        msglen = len(payload)
+        totalsent = 0
+        while totalsent < msglen:
+            sent = clientsocket.send(payload[totalsent:].encode())
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+        if arr[0] == "0":
+            print("PK:"+arr[1])
+        elif arr[0] == "1":
+            print("SK:"+arr[1])
+        elif arr[0] == "2":
+            print("PKS:"+arr[1])
+        elif arr[0] == "3":
+            msgId = int(arr[1][:4], 16)
+            print("[CLIENT] " + str(msgId))
+        elif arr[0] == "4":
+            msgId = int(arr[1][:4], 16)
+            print("[SERVER] " + str(msgId) + " " + str(totalsent))
+        time.sleep(50.0 / 1000.0)
 
 
 def instrument_debugger_checks():
@@ -57,10 +69,11 @@ def runCmd(cmd):
 if not os.path.exists("dumps"):
     os.makedirs("dumps")
 
-#sess_file = open("dumps/" + str(time.time()) + ".bin", "w")
+sess_file = open("dumps/" + str(time.time()) + ".bin", "w")
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('localhost', 10101))
+if mode == "0":
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect(('localhost', 10101))
 
 runCmd("adb shell am force-stop " + package_name)
 runCmd("adb shell am start -n " + package_name + "/" + package_name + ".GameApp")
@@ -70,4 +83,5 @@ script = process.create_script(instrument_debugger_checks())
 script.on('message', parse_message)
 script.load()
 sys.stdin.read()
-#sess_file.close()
+if mode == "1":
+    sess_file.close()
