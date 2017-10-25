8cbec9c1bb5633d37a91401484018db511cd09be
iGio90 <giovanni.rocca.90@gmail.com>
2017-10-25 16:44:10 +0200

    Added license, style dumper, small fixes + readme

--- dumper.py
+++ dumper.py
@@ -1,3 +1,16 @@
+# GDumper is a free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import frida
 import json
 import socket
@@ -6,6 +19,7 @@ import time
 import os
 import argparse
 
+
 def parse_message(message, data):
     payload = message["payload"]
     arr = payload.split("::::")
@@ -13,15 +27,15 @@ def parse_message(message, data):
 
     if mode == "dump":
         if arr[0] == "0":
-            print("PK:"+arr[1])
+            print("PK:" + arr[1])
             sess_file = open(path + "/pk" + ".bin", "w")
             sess_file.write(arr[1])
         elif arr[0] == "1":
-            print("SK:"+arr[1])
+            print("SK:" + arr[1])
             sess_file = open(path + "/sk" + ".bin", "w")
             sess_file.write(arr[1])
         elif arr[0] == "2":
-            print("PKS:"+arr[1])
+            print("PKS:" + arr[1])
             sess_file = open(path + "/pks" + ".bin", "w")
             sess_file.write(arr[1])
         elif arr[0] == "3":
@@ -34,7 +48,7 @@ def parse_message(message, data):
             sess_file = open(path + "/" + str(int(round(time.time() * 1000))) + "_server_" + str(msgId) + ".bin", "w")
             sess_file.write(arr[1])
             print("[SERVER] " + str(msgId))
-        if (sess_file is not None):
+        if sess_file is not None:
             sess_file.close()
     elif mode == "proxy":
         msglen = len(payload)
@@ -45,11 +59,11 @@ def parse_message(message, data):
                 raise RuntimeError("socket connection broken")
             totalsent = totalsent + sent
         if arr[0] == "0":
-            print("PK:"+arr[1])
+            print("PK:" + arr[1])
         elif arr[0] == "1":
-            print("SK:"+arr[1])
+            print("SK:" + arr[1])
         elif arr[0] == "2":
-            print("PKS:"+arr[1])
+            print("PKS:" + arr[1])
         elif arr[0] == "3":
             msgId = int(arr[1][:4], 16)
             print("[CLIENT] " + str(msgId))
@@ -58,27 +72,27 @@ def parse_message(message, data):
             print("[SERVER] " + str(msgId) + " " + str(totalsent))
         time.sleep(50.0 / 1000.0)
 
+
 def get_table():
     with open('xptable.json') as data_file:
         return json.load(data_file)
 
+
 def instrument_debugger_checks():
     injector = open("dumper.js", "r").read()
-    
-    cputype = ""
-    
-    if(arch == "arm"):
+
+    if arch == "arm":
         cputype = "1"
     else:
         cputype = "0"
-        
+
     gametype = ""
-    
-    if(game == "bb"):
+
+    if game == "bb":
         gametype = "0"
-    elif(game == "coc"):
+    elif game == "coc":
         gametype = "1"
-    
+
     xptable = get_table()[gametype][cputype]
     xp1 = int(xptable["1"], 16)
     xp2 = int(xptable["2"], 16)
@@ -90,9 +104,11 @@ def instrument_debugger_checks():
         xp3 = xp3 + 1
     return injector.replace("xxp1", str(xp1)).replace("xxp2", str(xp2)).replace("xxp3", str(xp3))
 
+
 def runCmd(cmd):
     os.system(cmd)
-    
+
+
 mode = ""
 game = ""
 arch = ""
@@ -105,35 +121,34 @@ parser = argparse.ArgumentParser(description='Dump SC protocol data.')
 parser.add_argument('-m', '--mode', help="Mode for dumper to run.")
 parser.add_argument('-g', '--game', help="Game to run.")
 parser.add_argument('-a', '--arch', help="Device architecture.")
-    
+
 args = parser.parse_args()
 
 mode = args.mode
 game = args.game
 arch = args.arch
 
-if(not mode or not game or not arch):
+if not mode or not game or not arch:
     print("Run with argument --help (-h) for more information.")
     sys.exit(1)
 
-if(mode != "proxy" and mode != "dump"):
+if mode != "proxy" and mode != "dump":
     print("Mode \"" + mode + "\" is not valid. Try \"proxy\" or \"dump\"")
     print("Proxy: Connects to the local node proxy server.")
     print("Dump: Starts a dump session.")
     sys.exit(1)
-    
-if(game != "coc" and game != "bb"):
+
+if game != "coc" and game != "bb":
     print("Game \"" + game + "\" is not valid. Try \"coc\" or \"bb\"")
     print("COC: Clash of Clans")
     print("BB: Boom Beach")
     sys.exit(1)
-        
-if(arch != "arm" and arch != "x86"):
+
+if arch != "arm" and arch != "x86":
     print("CPU Architecture \"" + arch + "\" is not valid. Try \"arm\" or \"x86\"")
     print("ARM: Recommended for most physical devices.")
     print("x86: Recommended for a few physical devices and most emulators.")
     sys.exit(1)
-
 
 if game == "bb":
     package_name = "com.supercell.boombeach"
@@ -141,7 +156,7 @@ elif game == "coc":
     package_name = "com.supercell.clashofclans"
 
 print("Starting dumper for " + package_name)
-	
+
 if not os.path.exists("dumps"):
     os.makedirs("dumps")
 
@@ -155,9 +170,9 @@ elif mode == "dump":
     path = "dumps/" + package_name + "_" + t
     os.makedirs(path)
 
-print ("Killing " + package_name)
+print("Killing " + package_name)
 runCmd("adb shell am force-stop " + package_name)
-print ("Starting " + package_name)
+print("Starting " + package_name)
 runCmd("adb shell am start -n " + package_name + "/" + package_name + ".GameApp")
 
 process = frida.get_usb_device().attach(package_name)
